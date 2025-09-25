287df38770350edbaeb40a6842ad3e75d828c922
Žiga Breznikar <itsziggy18@gmail.com>
2025-09-25 21:59:53 +0200

    Removed system tests specific files

--- create_warehouse/system_tests.py
+++
@@ -1,6 +0,0 @@
-from CreateWarehouse import CreateWarehouse
-
-warehouse = CreateWarehouse(parent_directory="system_tests",warehouse_name="warehouse_test",save_to_text=True)
-
-#warehouse.create_empty_warehouse()
-warehouse.create_pickup_scenario(pickup_scenario="scenario_test_4")
